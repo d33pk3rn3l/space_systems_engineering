135b1cd313f41c15820ecc4a5c9d8832c358e573
Leon Thomm <l.thomm@mailbox.org>
2025-05-26 16:36:38 +0200

    basic nix build

---
+++ README.md
@@ -0,0 +1,13 @@
+## Editing
+
+To alter the contents of the pages, edit the markdown files in `content/`.
+
+## Development
+
+To make more fundamental changes and build the site yourself, use the Nix file. Run `nix-shell` to get a shell with the development tools, or run `nix-build` to build the site.
+
+TODO: codespaces
+
+## Deployment
+
+TODO
---
+++ default.nix
@@ -0,0 +1,67 @@
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/412e75e351de85d98baf9124f45a8944b9278c58.tar.gz") {};
+  quartz-src = pkgs.fetchFromGitHub {
+    owner = "jackyzha0";
+    repo = "quartz";
+    rev = "v4.0.8";
+    sha256 = "sha256-bdn3ovklgAZt1mlYSofEwAjb6j4EAlZGK0ie1AeR9do=";
+  };
+  # pre-fetch dependencies for the quartz build to avoid
+  # network dependency when building the site below
+  quartz-deps = pkgs.buildNpmPackage {
+    pname = "quartz-deps";
+    version = "4.0.8";
+    src = quartz-src;
+    npmDepsHash = "sha256-H+G9KAn8PXtGM81TpHjNrmfWrORI4e/fwFLZqR+E5Ls=";
+    npmFlags = [ "--ignore-scripts" ];
+    dontNpmBuild = true;
+    installPhase = ''
+      mkdir -p $out
+      cp -r node_modules $out/
+      cp package*.json $out/
+    '';
+  };
+in
+pkgs.stdenv.mkDerivation {
+  name = "sse-site";
+  src = ./.;
+  buildInputs = [
+    pkgs.nodejs_24
+  ];
+  buildPhase = ''
+    cd $TMPDIR
+
+    export HOME=$(pwd)
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.musl ]}"
+    
+    # Fetch quartz
+    cp -r ${quartz-src} ./quartz
+    chmod -R u+w ./quartz
+
+    # Copy pre-built dependencies
+    cp -r ${quartz-deps}/node_modules ./quartz/
+    cp ${quartz-deps}/package*.json ./quartz/
+
+    # Copy markdown files
+    cp -r $src/content/* ./quartz/content
+
+    # Build the site
+    cd quartz
+    # `npx quartz build` will not work, so we invoke it manually:
+    node quartz/bootstrap-cli.mjs build
+  '';
+  installPhase = ''
+    mkdir -p $out
+
+    # Copy site artifacts
+    cp -r public/* $out/
+  '';
+  shellHook = ''
+    echo "for manual development:"
+    echo "  * git clone https://github.com/jackyzha0/quartz"
+    echo "  * cp -r content/* quartz/content/"
+    echo "  * cd quartz"
+    echo "  * npm install"
+    echo "  * node quartz/bootstrap-cli.mjs build --serve"
+  '';
+}
